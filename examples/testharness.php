f59f7e219afac78576cb9bd4481582a706177781
Ian Low <ianlow28@gmail.com>
2022-08-23 04:39:15 +0100

    Cyntag ymrwymiad

---
+++ examples/testharness.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("../vendor/autoload.php");
+
+$oprawf1 = new Ianl28\GeirfaDarllen\GeirfaDarllen();
+
+$oprawf1->testrun();
+
+?>
---
+++ src/GeirfaDarllen.php
@@ -0,0 +1,32 @@
+<?php
+/* ==============================================
+TO DO:
+======
+- details
+-------------------------------------------------
+DONE:
+=====
+# Changelog
+## 1.0.0 - yyyy-mm-dd
+### Changed
+- details
+### Fixed
+- details
+### Added
+- details
+================================================= */
+namespace Ianl28\GeirfaDarllen;
+class GeirfaDarllen{
+
+  //-------------------------------
+  public function __construct(){
+    echo "This is called from GeirfaDarllen->__construct()\n";
+  }//endfunc
+  //-------------------------------
+  public function testrun(){
+    echo "This is called from GeirfaDarllen->testrun()\n";
+  }//endfunc
+  //-------------------------------
+
+}//endclass 
+?>
